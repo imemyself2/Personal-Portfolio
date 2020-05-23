fb40bf22ca8551265264d9a6ed61039940ffac14
imemyself2 <dkaneria@purdue.edu>
2020-05-24 00:42:29 +0530

    Border remaining for navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,17 @@
 var i = 0; 
 var txt = 'DARSHIL KANERIA';
-var speed = 50;
+var speed = 70;
+var compsci = document.getElementById("compsci");
+var college = document.getElementById("college");
+var navbar = document.getElementById("navbar");
+
 
 window.onload = function() {
 
     typeWriter();
+    this.compsci.style.opacity = 1;
+    this.college.style.opacity = 1;
+    this.navbar.style.opacity = 1;
 
 }
 
