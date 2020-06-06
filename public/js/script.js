cba6c2251a9927923ef7d44489238a677f5a980f
imemyself2 <dkaneria@purdue.edu>
2020-06-06 12:30:45 +0530

    Modified about page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 var i = 0; 
 var txt = 'DARSHIL KANERIA';
-var speed = 70;
+var speed = 100;
 var compsci = document.getElementById("compsci");
 var college = document.getElementById("college");
 var navbar = document.getElementById("navbar");
@@ -33,4 +33,91 @@ function typeWriter() {
         setTimeout(typeWriter, speed);
     }
 }
+
+class TextScramble {
+    constructor(el) {
+      this.el = el
+    //   this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.chars = '_0101-'
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+}
+  
+const phrases = [
+    'Resume',
+    'Github',
+    'LinkedIn',
+ ]
+  
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+let counter = 0;
+fx.setText(phrases[counter]).then(() => {});
+document.getElementsByClassName('border')[0].addEventListener('click', function (event) {
+    if(phrases[counter] == 'Resume'){
+        window.open('https://www.google.com', "_blank");
+    }
+    else if(phrases[counter] == "Github"){
+        window.open('https://www.github.com/imemyself2', "_blank");
+    }
+    else if(phrases[counter] == "LinkedIn") {
+        window.open('https://www.linkedin.com/in/darshilkaneria', "_blank");
+    }
+});
+
+document.getElementsByClassName('right-arrow')[0].addEventListener('click', function (event) {
+    counter = (counter + 1) % phrases.length;
+    fx.setText(phrases[counter]).then(() => {});
+});
+
+document.getElementsByClassName('left-arrow')[0].addEventListener('click', function (event) {
+    counter = (counter - 1) % phrases.length;
+    fx.setText(phrases[counter]).then(() => {});
+});
 
